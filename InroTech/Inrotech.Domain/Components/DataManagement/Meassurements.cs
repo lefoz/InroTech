076cf104221314a14a139176efec59bf9c933bd8
Martin Jensby <lefoz.jensby@gmail.com>
2017-05-02 20:13:24 +0200

    connection to react webpage

---
+++ InroTech/Inrotech.Domain/Components/DataManagement/Meassurements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inrotech.Domain.Components.DataManagement
+{
+    class Meassurements
+    {
+        public int Temperature { get; set; }
+    }
+}
---
+++ InroTech/WebAPI/Helpers/MeassurementHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Helpers
+{
+    public class MeassurementHelper
+    {
+        //public static string GetMeassurements()
+        //{
+            
+        //}
+    }
+}
