e3b8e6e901d4a8b63afb3c16b14150557253293f
ole scharling <scharling@mensa.dk>
2017-05-12 13:07:01 +0200

    oprettet ny usercontroller til api

---
+++ InroTech/Inrotech.Domain/Database/Sim_database.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inrotech.Domain.Graph
+{
+    public class Sim_database
+    {
+        int data;
+        Random rand = new Random();
+
+        public Sim_database()
+        {
+
+        }
+        public int GetSim_database()
+        {
+            data = rand.Next(100);
+            return data;
+        }
+    }
+}
--- InroTech/WebAPI/Controllers/ValuesController.cs
+++ InroTech/WebAPI/Controllers/ValuesController.cs
@@ -22,23 +22,39 @@ namespace WebAPI.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value "+id;
         }
+        //http://nodogmablog.bryanhogan.net/2016/01/asp-net-5-web-api-controller-with-multiple-get-methods/
+        //http://stackoverflow.com/questions/38109927/optional-int-parameter-in-web-api-attribute-routing
+        // GET api/values/5
+        [HttpGet("getmyvalues/{id}")]
+        public string GetValues(int id)
+        {
+            return "my value: "+id;
+        }
+
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            System.Console.WriteLine("my value; value");
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
-        }
+    //         private static DbConnector DbConnectorInstance;
+    //    private const String ConnectorString = @"server=mysql45.unoeuro.com;Port=3306;userid=kasper_mad_com;
+    //password=Gruppe3;database=kasper_madsen_com_db";
+    //    MySqlConnection conn;
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
+
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
