52a5b24966d2af2208b1470e3b305db48979feab
doubtingparis <doubting_paris@hotmail.com>
2017-05-18 10:00:30 +0200

    testapp

--- InroTech/Inrotech.Domain/Components/Robot/Robot.cs
+++ InroTech/Inrotech.Domain/Components/Robot/Robot.cs
@@ -7,17 +7,17 @@ namespace Inrotech.Domain.Components.Robot
 {
     public class Robot
     {
-        public string hostName;
-
         private FRRJIf.Core objCore;
-        private FRRJIf.DataTable objDataTable;
+        private FRRJIf.DataTable objDataTable1;
         private FRRJIf.DataTable objDataTable2;
         private FRRJIf.DataNumReg objNumReg;
         private FRRJIf.DataNumReg objNumReg2;
         private FRRJIf.DataNumReg objNumReg3;
+        private FRRJIf.DataTask[] objTask;
 
+        public string hostName;
 
-        private void connect(String targetHost)
+        private void startConnect(String targetHost)
         {
             hostName = targetHost;
 
@@ -29,6 +29,7 @@ namespace Inrotech.Domain.Components.Robot
             else
             {
                 //disconnect
+                Console.WriteLine("Disconnected");
                 objCore.Disconnect();
             }
         }
@@ -36,11 +37,15 @@ namespace Inrotech.Domain.Components.Robot
         private void refresh()
         {
             int ii = 0;
+            short intLine = 0, intState = 0;
             string strResult = null;
             bool blnValidDT = false;
             object vntValue = null;
+            string strProg = "", strParentProg = "";
+            Array lngAI = new int[3];
+            Array lngAO = new int[3];
 
-            blnValidDT = objDataTable.Refresh();
+            blnValidDT = objDataTable1.Refresh();
             //if refresh failed, disconnect
             if(blnValidDT == false)
             {
@@ -49,53 +54,100 @@ namespace Inrotech.Domain.Components.Robot
                 return;
             }
 
-            //data printout
-            strResult = strResult + "--- NumReg ---\r\n";
+            //TESTDATA FORMAT
+            //Numregs
+            strResult = strResult + "--- NumRegs ---\r\n";
+            for (ii = objNumReg.StartIndex; ii <= objNumReg.EndIndex; ii++)
             {
-                for (ii = objNumReg.StartIndex; ii <= objNumReg.EndIndex; ii++)
+                if (objNumReg.GetValue(ii, ref vntValue) == true)
                 {
-                    if (objNumReg.GetValue(ii, ref vntValue) == true)
-                    {
-                        strResult = strResult + "R[" + ii + "] = " + vntValue + "\r\n";
-                    }
-                    else
-                    {
-                        strResult = strResult + "R[" + ii + "] : Error!!! \r\n";
-                    }
+                    strResult = strResult + "R[" + ii + "] = " + vntValue + "\r\n";
+                }
+                else
+                {
+                    strResult = strResult + "R[" + ii + "] : Error!!! \r\n";
+                }
+            }        
+            for (ii = objNumReg2.StartIndex; ii <= objNumReg2.EndIndex; ii++)
+            {
+                if (objNumReg2.GetValue(ii, ref vntValue) == true)
+                {
+                    strResult = strResult + "R[" + ii + "] = " + vntValue + "\r\n";
+                }
+                else
+                {
+                    strResult = strResult + "R[" + ii + "] : Error!!! \r\n";
                 }
             }
+            //Tasks
+            strResult = strResult + "--- Tasks ---\r\n";
+            for (int i = objTask.GetLowerBound(0); i <= objTask.GetUpperBound(0); i++)
             {
-                for (ii = objNumReg2.StartIndex; ii <= objNumReg2.EndIndex; ii++)
+                if (objTask[i].GetValue(ref strProg, ref intLine, ref intState, ref strParentProg))
                 {
-                    if (objNumReg2.GetValue(ii, ref vntValue) == true)
+                    if (strProg == "")
                     {
-                        strResult = strResult + "R[" + ii + "] = " + vntValue + "\r\n";
-                    }
-                    else
-                    {
-                        strResult = strResult + "R[" + ii + "] : Error!!! \r\n";
-                    }
+                        break;
+                    }                        
+                    strResult = strResult + StrTask(objTask[i].Index, strProg, intLine, intState, strParentProg);
+                }
+                else
+                {
+                    strResult = strResult + "Task error!!!\r\n";
                 }
             }
+            //Analog IO
+            //read AO - offset 1000 for AO
+            {
+                if (objCore.ReadGO(1000 + 1, ref lngAO, 2) == false)
+                {
+                    Console.WriteLine("Disconnected - read AO fail");
+                    objCore.Disconnect();
+                    return;
+                }
+                //read AI - offset 1000 for AI
+                if (objCore.ReadGI(1000 + 1, ref lngAI, 2) == false)
+                {
+                    Console.WriteLine("Disconnected - read AI fail");
+                    objCore.Disconnect();
+                    return;
+                }
+                strResult = strResult + "--- AO ---" + "\r\n";
+                strResult = strResult + StrIO("AO", 1, 2, ref lngAO) + "\r\n";
+                strResult = strResult + "--- AI ---" + "\r\n";
+                strResult = strResult + StrIO("AI", 1, 2, ref lngAI) + "\r\n";
+            }                
             Console.WriteLine(strResult);
         }
 
-        //initialize
+        //INITIALIZE, CONNECT, CONSTRUCT DATATABLES
         private void subInit()
         {
             bool connectSuccess = false;
             string strHost = null;
-            int timeOut = 0;
+            int timeOut = 5; //connection timeout value
 
             try
             {
+                //Set core
                 objCore = new FRRJIf.Core();
-
-                // You need to set data table before connecting.
-                objDataTable = objCore.get_DataTable();
+                //Set datatable1
+                objDataTable1 = objCore.get_DataTable();
                 {                    
-                    objNumReg = objDataTable.AddNumReg(FRRJIf.FRIF_DATA_TYPE.NUMREG_INT, 1, 5);
-                    objNumReg2 = objDataTable.AddNumReg(FRRJIf.FRIF_DATA_TYPE.NUMREG_REAL, 6, 10);
+                    objNumReg = objDataTable1.AddNumReg(FRRJIf.FRIF_DATA_TYPE.NUMREG_INT, 1, 5);
+                    objNumReg2 = objDataTable1.AddNumReg(FRRJIf.FRIF_DATA_TYPE.NUMREG_REAL, 6, 10);
+
+                    objTask = new FRRJIf.DataTask[10];
+                    objTask[0] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 1);
+                    objTask[1] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 2);
+                    objTask[2] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 3);
+                    objTask[3] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 4);
+                    objTask[4] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 5);
+                    objTask[5] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 6);
+                    objTask[6] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 7);
+                    objTask[7] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 8);
+                    objTask[8] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 9);
+                    objTask[9] = objDataTable1.AddTask(FRRJIf.FRIF_DATA_TYPE.TASK, 10);
                 }
 
                 // 2nd data table.
@@ -103,7 +155,7 @@ namespace Inrotech.Domain.Components.Robot
                 objDataTable2 = objCore.get_DataTable2();
                 objNumReg3 = objDataTable2.AddNumReg(FRRJIf.FRIF_DATA_TYPE.NUMREG_INT, 1, 5);
                 
-                //get host name
+                //HOSTNAME ON LOCAL NETWORK
                 if (string.IsNullOrEmpty(hostName))
                 {
                     Console.WriteLine("Write hostname and press enter:");
@@ -115,20 +167,21 @@ namespace Inrotech.Domain.Components.Robot
                     strHost = hostName;
                 }
 
-                //time out value
-                timeOut = 5;
-
-                //connect
+                //CONNECTING
+                timeOut = 5; //time out value                
                 if (timeOut > 0)
                     objCore.set_TimeOutValue(timeOut);
                 connectSuccess = objCore.Connect(strHost);
                 if (connectSuccess == false)
                 {
                     //disconnected
+                    Console.WriteLine("Disconnected");
+                    objCore.Disconnect();
                 }
                 else
                 {
                     //connected
+                    Console.WriteLine("Connected");
                 }
                 return;
             }
@@ -140,15 +193,45 @@ namespace Inrotech.Domain.Components.Robot
         }
 
 
-        static void Main(string[] args)
+        //DATA TYPES
+        private string StrTask(int Index, string strProg, short intLine, short intState, string strParentProg)
         {
-            Robot r = new Robot();
+            string tmp = null;
+
+            tmp = "TASK" + Index + ": ";
+            tmp = tmp + " Prog=\"" + strProg + "\"";
+            tmp = tmp + " Line=" + intLine;
+            tmp = tmp + " State=" + intState;
+            tmp = tmp + " ParentProg=\"" + strParentProg + "\"";
+
+            return tmp + "\r\n";
+        }
+        private string StrIO(string strIOType, short StartIndex, short EndIndex, ref Array values)
+        {
+            string tmp = null;
+            int i = 0;
+
+            tmp = strIOType + "[" + Convert.ToString(StartIndex) + "-" + Convert.ToString(EndIndex) + "]=";
+            for (i = 0; i <= EndIndex - StartIndex; i++)
+            {
+                if (i != 0)
+                {
+                    tmp = tmp + ",";
+                }
+                tmp = tmp + values.GetValue(i);
+            }
+            return tmp;
+        }
+
+    //TEST EXECUTION METHOD
+    public void StartTest(string IP)
+        {
             Console.WriteLine("Trying to connect");
-            r.connect("192.168.117.40");
+            startConnect(IP);
             Console.WriteLine("Sleeping for 500ms");
             System.Threading.Thread.Sleep(500);
             Console.WriteLine("Refresh prompted");
-            r.refresh();
+            refresh();
         }
     }
 }
---
+++ InroTech/TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Inrotech.Domain.Components.Robot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot r = new Robot();
+            r.StartTest("192.168.117.40");
+            Console.ReadKey();
+        }
+    }
+}
