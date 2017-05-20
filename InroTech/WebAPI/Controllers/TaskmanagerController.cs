86d83e9d9cdf99e99c27f896631f539c6f4865e4
ole scharling <scharling@mensa.dk>
2017-05-20 14:18:11 +0200

    userDB

--- InroTech/WebAPI/Controllers/TaskmanagerController.cs
+++ InroTech/WebAPI/Controllers/TaskmanagerController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Inrotech.Domain.Sim;
+using Inrotech.Domain.UserDb;
 using System.Data;
 using Inrotech.Domain.Taskmanager;
 
