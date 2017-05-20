41405d8ac522336d7669a7aa7f0b02e1305be4c2
Martin Jensby <lefoz.jensby@gmail.com>
2017-05-20 13:23:04 +0200

    robot info

--- InroTech/Inrotech.Domain/Register/Sim_Register.cs
+++ InroTech/Inrotech.Domain/Register/Sim_Register.cs
@@ -21,17 +21,16 @@ namespace Inrotech.Domain.Register
             SimReg.Clear();
             SimReg.Columns.Add("id", typeof(int));
             SimReg.Columns.Add("Registry", typeof(int));
-            SimReg.Columns.Add("Name", typeof(string));
             SimReg.Columns.Add("Value", typeof(double));
             SimReg.Columns.Add("Selected", typeof(bool));
-            SimReg.Rows.Add(new object[] { 1, 025, "index 025", 500, false});
-            SimReg.Rows.Add(new object[] { 2, 055, "index 055", 25, false });
-            SimReg.Rows.Add(new object[] { 3, 075, "index 075", 50, false });
-            SimReg.Rows.Add(new object[] { 4, 125, "index 125", 960, false });
-            SimReg.Rows.Add(new object[] { 5, 138, "index 138", 58, false });
-            SimReg.Rows.Add(new object[] { 6, 285, "index 285", 74, false });
-            SimReg.Rows.Add(new object[] { 7, 789, "index 789", 35, false });
-            SimReg.Rows.Add(new object[] { 8, 358, "index 358", 40, false });
+            SimReg.Rows.Add(new object[] { 1, 025, 500, false});
+            SimReg.Rows.Add(new object[] { 2, 055, 25, false });
+            SimReg.Rows.Add(new object[] { 3, 075, 50, false });
+            SimReg.Rows.Add(new object[] { 4, 125, 960, false });
+            SimReg.Rows.Add(new object[] { 5, 138, 58, false });
+            SimReg.Rows.Add(new object[] { 6, 285, 74, false });
+            SimReg.Rows.Add(new object[] { 7, 789, 35, false });
+            SimReg.Rows.Add(new object[] { 8, 358, 40, false });
             return SimReg;
         }
 
@@ -92,6 +91,12 @@ namespace Inrotech.Domain.Register
             {
                 Console.WriteLine(item);
             }
+        }
+
+        public String[] Sim_RobotInfo()
+        {
+            String[] SimInfo = new string[]{"Simulate Robot", "localhost:53982","SimJob", "SimProgress" };
+            return SimInfo;
         }
     }
 }
