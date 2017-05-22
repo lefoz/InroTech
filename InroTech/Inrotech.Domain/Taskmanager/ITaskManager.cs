5ea61b0d7fb1990907b400fbd9100e8c8f999550
Madsen88 <kasper.madsen88@gmail.com>
2017-05-22 08:41:36 +0200

    Another good commit

--- InroTech/Inrotech.Domain/Taskmanager/ITaskManager.cs
+++ InroTech/Inrotech.Domain/Taskmanager/ITaskManager.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace Inrotech.Domain.Taskmanager
 {
-    public interface Itaskmanager
+    public interface ITaskmanager
     {
         DataTable getTaskmanager();
-        void SetTaskmanager(IRobot robot);
+        void SetTaskmanager();
     }
 }
