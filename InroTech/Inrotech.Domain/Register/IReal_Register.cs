a5bdf73ba03fd55683ab8a7500ff0f41f2a92c6d
doubtingparis <doubting_paris@hotmail.com>
2017-05-22 09:18:53 +0200

    ?

---
+++ InroTech/Inrotech.Domain/Register/IReal_Register.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Inrotech.Real_Register
+{
+    public interface IReal_Register
+    {
+        string[] GetAllReg();
+        DataTable GetReg();
+        DataTable GetSelectedReg(string[] selItems);
+        void RegArrayTester(string[] test);
+        string[] RobotInfo();
+    }
+}
