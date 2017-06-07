1fd30bf2354a4193d08bc2fb913a83e514f0768e
Martin Jensby <lefoz.jensby@gmail.com>
2017-06-07 11:28:38 +0200

    random Generator add to reg

--- InroTech/Inrotech.Domain/Register/Sim_Register.cs
+++ InroTech/Inrotech.Domain/Register/Sim_Register.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using Inrotech.Domain.Register;
 namespace Inrotech.Domain.Register
 {
@@ -9,6 +10,7 @@ namespace Inrotech.Domain.Register
     {
         private DataTable SimReg;
         private DataTable SimRegSelected;
+        private Random randomGen = new Random();
 
         public Sim_Register()
         {
@@ -23,14 +25,14 @@ namespace Inrotech.Domain.Register
             SimReg.Columns.Add("Registry", typeof(int));
             SimReg.Columns.Add("Value", typeof(double));
             SimReg.Columns.Add("Selected", typeof(bool));
-            SimReg.Rows.Add(new object[] { 1, 025, 500, false});
-            SimReg.Rows.Add(new object[] { 2, 055, 25, false });
-            SimReg.Rows.Add(new object[] { 3, 075, 50, false });
-            SimReg.Rows.Add(new object[] { 4, 125, 960, false });
-            SimReg.Rows.Add(new object[] { 5, 138, 58, false });
-            SimReg.Rows.Add(new object[] { 6, 285, 74, false });
-            SimReg.Rows.Add(new object[] { 7, 789, 35, false });
-            SimReg.Rows.Add(new object[] { 8, 358, 40, false });
+            SimReg.Rows.Add(new object[] { 1, 025, randomGen.Next(100), false});
+            SimReg.Rows.Add(new object[] { 2, 055, randomGen.Next(100), false });
+            SimReg.Rows.Add(new object[] { 3, 075, randomGen.Next(100), false });
+            SimReg.Rows.Add(new object[] { 4, 125, randomGen.Next(100), false });
+            SimReg.Rows.Add(new object[] { 5, 138, randomGen.Next(100), false });
+            SimReg.Rows.Add(new object[] { 6, 285, randomGen.Next(100), false });
+            SimReg.Rows.Add(new object[] { 7, 389, randomGen.Next(100), false });
+            SimReg.Rows.Add(new object[] { 8, 458, randomGen.Next(100), false });
             return SimReg;
         }
 
